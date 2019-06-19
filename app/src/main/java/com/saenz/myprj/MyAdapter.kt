3775f79e5d3aaa4c99df6b9b5ab912ebfae16bc4
00400815@uca.edu.sv <16137238>
2019-06-19 17:33:41 -0600

    initial

---
+++ app/src/main/java/com/saenz/myprj/MyAdapter.kt
@@ -0,0 +1,40 @@
+package com.saenz.myprj
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.list_items.view.*
+
+class MyAdapter(private val myDataset: List<Level_1>) :  RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(parent: ViewGroup,
+                                    viewType: Int): MyAdapter.MyViewHolder {
+        // create a new view
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_items, parent, false)
+        // set the view's size, margins, paddings and layout parameters
+
+        return MyViewHolder(view)
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        val sonidos = myDataset[position]
+        holder.setData( sonidos, position)
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount() = myDataset.size
+
+    class MyViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+        fun setData(sonidos:Level_1?,pos:Int){
+            itemView.txvTitle.text = sonidos!!.title
+
+        }
+    }
+}
